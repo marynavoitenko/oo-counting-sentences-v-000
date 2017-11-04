2292147e8653a33fd819189d7d03cda1d74efaf3
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-11-04 19:18:47 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -5,19 +5,28 @@ class String
   def sentence?
     if self.end_with?(".")
       return true
-    else
-      return false
+     else
+       return false
+     end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    self.split(/\.|!|\?/).delete_if { |str| str.empty? }.count
   end
 end
